beee9365f08a2f34156137fb1932f1bb66cb6520
shannonCg <phy8233@gmail.com>
2020-05-16 15:37:00 +0800

    add hadoop mapreduce practice

---
+++ MaxTemperature.java
@@ -0,0 +1,25 @@
+import java.io.FileInputStream;
+
+public class MaxTemperature {
+    public static void main(String[] args) throws Exception{
+        if(args.length != 2){
+            System.err.println("Usage: MaxTemperature <input path> <output path>");
+            System.exit(-1);
+        }
+
+        Job job = new Job();
+        job.setJarByClass(MaxTemperature.class);
+        job.setJobName("Max Temperature");
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        job.setMapperClass(MaxTemperatureMapper.class);
+        job.setReducerClass(MaxTemperatureReducer.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        System.exit(job.waitForCompletion(true)? 0 : 1);
+    }
+}
